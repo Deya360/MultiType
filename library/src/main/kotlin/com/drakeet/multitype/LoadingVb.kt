05e65e0e6e713071cd376dc4be3b7344f31be6b6
Deya360 <32934092+Deya360@users.noreply.github.com>
2021-02-10 16:56:22 +0300

    Amend to allow clazz to be nullable to support paging v3 library placeholder views

--- library/src/main/kotlin/com/drakeet/multitype/LoadingVb.kt
+++ library/src/main/kotlin/com/drakeet/multitype/LoadingVb.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-internal class LoadingVb: ItemViewDelegate<Loading, LoadingVb.ViewHolder>() {
+internal class LoadingVb: ItemViewDelegate<Loading?, LoadingVb.ViewHolder>() {
     override fun onCreateViewHolder(context: Context, parent: ViewGroup): ViewHolder = ViewHolder(View(context))
     override fun onBindViewHolder(holder: ViewHolder, item: Loading?) {}
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
--- library/src/main/kotlin/com/drakeet/multitype/Type.kt
+++ library/src/main/kotlin/com/drakeet/multitype/Type.kt
@@ -21,6 +21,6 @@ package com.drakeet.multitype
  */
 data class Type<T>(
   val clazz: Class<out T>,
-  val delegate: ItemViewDelegate<T, *>,
+  val delegate: ItemViewDelegate<T?, *>,
   val linker: Linker<T>
 )
